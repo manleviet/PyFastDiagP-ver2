55326b46f92c51be01fd196014911ece4dee21f1
Lê Viết Mẫn <manleviet@gmail.com>
2022-08-14 00:44:09 +0200

    add ejemplo.sh

---
+++ ejemplo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#---------------Script SBATCH - NLHPC ----------------
+#SBATCH -J FastDiag
+#SBATCH -p general
+#SBATCH -n 44
+#SBATCH --ntasks-per-node=44
+#SBATCH -c 1
+#SBATCH --mem-per-cpu=4250
+#SBATCH --mail-user=cvidal@utalca.cl
+#SBATCH --mail-type=ALL
+#SBATCH -o FastDiag_%j.out
+#SBATCH -e FastDiag_%j.err
+
+#-----------------Toolchain---------------------------
+# ----------------Modulos----------------------------
+# ----------------Comando--------------------------
+
+python evaluate_v2.py
